86617982710422667553ab4d6729f3a893e0dedf
RAYSSA GOULART <rayssa_goulart@estudante.sesisenai.org.br>
2024-09-09 14:10:06 -0300

    ajustando os test

--- aula-be-dev/testes/clientes.test.js
+++ aula-be-dev/testes/clientes.test.js
@@ -2,20 +2,25 @@ const request = require('supertest');
 const app = require('../index'); 
 
 describe('Clientes API', () => {
+    let clienteId;
 
-    let clienteId; 
-
-    beforeAll(async () => {
-       
-        const res = await request(app).post('/clientes').send({
-            nome: "Cliente Teste",
-            email: "cliente@example.com",
-            senha: "senha"
+    // Testar se esta criando clientes
+    describe('POST /clientes', () => {
+        it('deve criar um novo cliente com sucesso', async () => {
+            const res = await request(app).post('/clientes').send({
+                nome: "Cliente Teste",
+                email: "cliente@example.com",
+                senha: "senha"
+            });
+            expect(res.status).toBe(201); 
+            expect(res.body).toHaveProperty('id'); 
+            expect(res.body).toHaveProperty('nome', 'Cliente Teste');
+            expect(res.body).toHaveProperty('email', 'cliente@example.com');
+            clienteId = res.body.id; 
         });
-        clienteId = res.body.id;
     });
 
-    //testar se esta retornando a lista de clientes
+    //testa se esta listando lista de clientes
     describe('GET /clientes', () => {
         it('deve retornar a lista de clientes com sucesso', async () => {
             const res = await request(app).get('/clientes').send();
@@ -23,7 +28,7 @@ describe('Clientes API', () => {
             expect(res.body).toBeInstanceOf(Array);
         });
     });
-    
+
     //testar se esta listando um cliente especifico
     describe('GET /clientes/:id', () => {
         it('deve retornar um cliente específico pelo ID com sucesso', async () => {
@@ -35,57 +40,33 @@ describe('Clientes API', () => {
         it('deve retornar 404 para um cliente não encontrado', async () => {
             const res = await request(app).get('/clientes/999').send();
             expect(res.status).toBe(404);
-            expect(res.body.error).toBe('not found'); 
+            expect(res.body.error).toBe('not found');
         });
     });
 
     //testar se esta atualizando cliente
     describe('PUT /clientes/:id', () => {
-        describe('Atualizar um cliente existente', () => {
-            it('deve atualizar um cliente existente com sucesso', async () => {
-                const res = await request(app).put(`/clientes/${clienteId}`).send({
-                    nome: "Cliente Atualizado",
-                    email: "clienteatualizado@example.com"
-                });
-                expect(res.status).toBe(200);
-                expect(res.body).toHaveProperty('nome', 'Cliente Atualizado');
-                expect(res.body).toHaveProperty('email', 'clienteatualizado@example.com');
+        it('deve atualizar um cliente existente com sucesso', async () => {
+            const res = await request(app).put(`/clientes/${clienteId}`).send({
+                nome: "Cliente Atualizado",
+                email: "clienteatualizado@example.com"
             });
-        });
-
-    });
-
-    //testar se esta criando um cliente
-    describe('POST /clientes', () => {
-        it('deve criar um novo cliente com sucesso', async () => {
-            const res = await request(app).post('/clientes').send({
-                nome: "Cliente Teste",
-                email: "cliente@example.com",
-                senha: "senha"
-            });
-            expect(res.status).toBe(201); // Assumindo que a criação deve retornar um status 201 Created
-            expect(res.body).toHaveProperty('id'); // Verifica se o ID do cliente foi retornado
-            expect(res.body).toHaveProperty('nome', 'Cliente Teste');
-            expect(res.body).toHaveProperty('email', 'cliente@example.com');
-            clienteId = res.body.id; // Armazena o ID do cliente criado para usar nos outros testes
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('nome', 'Cliente Atualizado');
+            expect(res.body).toHaveProperty('email', 'clienteatualizado@example.com');
         });
     });
 
-    
-    //testar se esta deletando um cliente
+    //testar se esta deletando cliente
     describe('DELETE /clientes/:id', () => {
-        describe('Deletar um cliente existente', () => {
-            it('deve deletar um cliente existente com sucesso', async () => {
-                const res = await request(app).delete(`/clientes/${clienteId}`).send();
-                expect(res.status).toBe(204);
-            });
+        it('deve deletar um cliente existente com sucesso', async () => {
+            const res = await request(app).delete(`/clientes/${clienteId}`).send();
+            expect(res.status).toBe(204);
         });
-
-        
     });
 
     afterAll(async () => {
-        // Remove o cliente criado para os testes
+        
         if (clienteId) {
             await request(app).delete(`/clientes/${clienteId}`);
         }
--- aula-be-dev/testes/produtos.test.js
+++ aula-be-dev/testes/produtos.test.js
@@ -1,21 +1,28 @@
 const request = require('supertest');
-const app = require('../index'); // Certifique-se de que o caminho está correto
+const app = require('../index'); 
 
 describe('Produtos API', () => {
+    let produtoId;
 
-    let produtoId; 
-
-    beforeAll(async () => {
-        
-        const res = await request(app).post('/produtos').send({
-            nome: "Produto Teste",
-            descricao: "Descrição do produto teste",
-            preco: 100.00
+    
+    //testar se esta criando produto
+    describe('POST /produtos', () => {
+        it('deve criar um novo produto com sucesso', async () => {
+            const res = await request(app).post('/produtos').send({
+                nome: "Produto Teste",
+                preco: 100.00,
+                descricao: "Descrição do Produto Teste"
+            });
+            expect(res.status).toBe(201); 
+            expect(res.body).toHaveProperty('id'); 
+            expect(res.body).toHaveProperty('nome', 'Produto Teste');
+            expect(res.body).toHaveProperty('preco', 100.00);
+            expect(res.body).toHaveProperty('descricao', 'Descrição do Produto Teste');
+            produtoId = res.body.id; // Armazena o ID do produto criado para usar nos outros testes
         });
-        produtoId = res.body.id;
     });
 
-    //testar listar todos os produtos
+    //listar produtos
     describe('GET /produtos', () => {
         it('deve retornar a lista de produtos com sucesso', async () => {
             const res = await request(app).get('/produtos').send();
@@ -23,8 +30,7 @@ describe('Produtos API', () => {
             expect(res.body).toBeInstanceOf(Array);
         });
     });
-
-    //testar listar produto especifico
+    //listar produto especifico
     describe('GET /produtos/:id', () => {
         it('deve retornar um produto específico pelo ID com sucesso', async () => {
             const res = await request(app).get(`/produtos/${produtoId}`).send();
@@ -35,58 +41,35 @@ describe('Produtos API', () => {
         it('deve retornar 404 para um produto não encontrado', async () => {
             const res = await request(app).get('/produtos/999').send();
             expect(res.status).toBe(404);
-            expect(res.body.error).toBe('not found'); // Ajustado para "not found"
+            expect(res.body.error).toBe('not found');
         });
     });
 
-    //testar atualizar produto 
+    //atualizar produto
     describe('PUT /produtos/:id', () => {
-        describe('Atualizar um produto existente', () => {
-            it('deve atualizar um produto existente com sucesso', async () => {
-                const res = await request(app).put(`/produtos/${produtoId}`).send({
-                    nome: "Produto Atualizado",
-                    descricao: "Descrição atualizada",
-                    preco: 120.00
-                });
-                expect(res.status).toBe(200);
-                expect(res.body).toHaveProperty('nome', 'Produto Atualizado');
-                expect(res.body).toHaveProperty('preco', 120.00);
+        it('deve atualizar um produto existente com sucesso', async () => {
+            const res = await request(app).put(`/produtos/${produtoId}`).send({
+                nome: "Produto Atualizado",
+                preco: 150.00,
+                descricao: "Descrição Atualizada"
             });
-        });
-
-    });
-
-    //testar se esta criando produto
-    describe('POST /produtos', () => {
-        it('deve criar um novo produto com sucesso', async () => {
-            const res = await request(app).post('/produtos').send({
-                nome: "Produto Teste",
-                preco: 100.00,
-                descricao: "Descrição do Produto Teste"
-            });
-            expect(res.status).toBe(201); // Assumindo que a criação deve retornar um status 201 Created
-            expect(res.body).toHaveProperty('id'); // Verifica se o ID do produto foi retornado
-            expect(res.body).toHaveProperty('nome', 'Produto Teste');
-            expect(res.body).toHaveProperty('preco', 100.00);
-            expect(res.body).toHaveProperty('descricao', 'Descrição do Produto Teste');
-            produtoId = res.body.id; // Armazena o ID do produto criado para usar nos outros testes
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty('nome', 'Produto Atualizado');
+            expect(res.body).toHaveProperty('preco', 150.00);
+            expect(res.body).toHaveProperty('descricao', 'Descrição Atualizada');
         });
     });
 
-    //testar deletar produto
+    //deletar produto
     describe('DELETE /produtos/:id', () => {
-        describe('Deletar um produto existente', () => {
-            it('deve deletar um produto existente com sucesso', async () => {
-                const res = await request(app).delete(`/produtos/${produtoId}`).send();
-                expect(res.status).toBe(204);
-            });
+        it('deve deletar um produto existente com sucesso', async () => {
+            const res = await request(app).delete(`/produtos/${produtoId}`).send();
+            expect(res.status).toBe(204);
         });
-
-        
     });
 
     afterAll(async () => {
-        // Remove o produto criado para os testes
+        
         if (produtoId) {
             await request(app).delete(`/produtos/${produtoId}`);
         }
